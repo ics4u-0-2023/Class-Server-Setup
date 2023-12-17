50adef7dcbf6d9f55fa72123c46aa5ad9314d488
Patrick Coxall <mr.coxall@mths.ca>
2023-12-17 16:14:41 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ systemctl start ssh
 
 # NeoVim
 cd /tmp
-apt-get install ninja-build gettext cmake unzip curl
+apt-get install ninja-build gettext cmake unzip curl -y
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 make install
